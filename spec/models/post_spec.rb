29a25d269eea0b0a6c87c0618fbe58c6ab31da92
ReneeVL85 <renee.v.leijen@gmail.com>
2016-05-10 15:19:47 +0200

    added validations to post model and rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Post, type: :model do
   describe "validations" do
      it { is_expected.to belong_to(:user) }
      it { is_expected.to have_many(:comments) }
+     it { is_expected.to validate_presence_of(:user) }
+     it { is_expected.to validate_presence_of(:content) }
    end
 end
