d53c7a33b92eb82cd84efb38d2e8d145c140e0aa
ReneeVL85 <renee.v.leijen@gmail.com>
2016-05-10 14:31:55 +0200

    added rspec to user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+ describe "validations" do
+    it { is_expected.to have_many(:posts) }
+    it { is_expected.to have_many(:comments) }
+  end
+end
