c37c5761b6a5f2431c701993222b928d3dd1b656
ReneeVL85 <renee.v.leijen@gmail.com>
2016-05-10 10:34:56 +0200

    tested basic posts routing with Rspec

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe PostsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/").to route_to("posts#index")
+      expect(:get => "/").to route_to("welcome#index")
     end
 
     it "routes to #create" do
       expect(:post => "/posts").to route_to("posts#create")
     end
+
+    it "routes to #show" do
+      expect(:get =>"/posts/1").to route_to("posts#show", id: "1")
+    end
+
+  end
 end
